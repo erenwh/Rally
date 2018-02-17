6995eec9a924f37acb2636fd6285c0f0a6883f56
Elijah Hauber <ethauber@outlook.com>
2018-02-16 20:50:26 -0500

    Revert "Revert "changes to jasmine files""
    This reverts commit 34e64114de7c6e38eaf2a3f9872cefe3053de6b0.

--- spec/RegisterSpec.js
+++ spec/RegisterSpec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import register from './register.vue'
+import register from '@/components/entry/register'
 
-describe("Register", function() {
+describe("register.vue", function() {
     var user = "brad";
     var password = "password";
     var email = "brad@brad.br";
@@ -9,7 +9,7 @@ describe("Register", function() {
     it('has a created hook', function(){
         expect(typeof register.submit).toBe('function');
     });
-    it("should create a new user", function(){
+    /*it("should create a new user", function(){
         expect(typeof register.submit).toBe('function');
-    });
+    });*/
 });
