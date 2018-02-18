4c80ca76f6c5bdf20c0ea7b2ee8fba8509726e6d
Elijah Hauber <ethauber@outlook.com>
2018-02-18 12:10:35 -0500

    more advanced testing for register

--- test/unit/specs/register.spec.js
+++ test/unit/specs/register.spec.js
@@ -27,4 +27,21 @@ describe('register.vue', () => {
         assert.typeOf(registerComponent.google, 'function');
     })
   
+    it('tests form input and submit button click', () => {
+        const Constructor = Vue.extend(register);
+        const registerComponent = new Constructor().$mount();
+        //insert information
+        registerComponent.username = 'john';
+        registerComponent.email = 'john@john.com';
+        registerComponent.password = 'johnjohn';
+        registerComponent.Conpassword = 'johnjohn';
+        // simulate click event
+        // buttons: 1st facebook, 2nd google, 3rd twitter, 4th submit
+        const button = registerComponent.$el.querySelectorAll('button')[3];
+        const clickEvent = new window.Event('click');
+        button.dispatchEvent(clickEvent);
+        registerComponent._watcher.run();
+        expect(registerComponent.username).to.contain('john');
+        expect(registerComponent.email).to.contain('john@john.com');
+    })
 })
