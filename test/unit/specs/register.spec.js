2f2420b3ae45abefb5a357ce6f897d0f5af19f65
HanWang <erenwh@gmail.com>
2018-03-08 16:21:59 -0500

    Revert to Pritesh commit

--- test/unit/specs/register.spec.js
+++ test/unit/specs/register.spec.js
@@ -3,6 +3,29 @@ import Vue from 'vue';
 
 describe('register.vue', () => {
 
+    it('has a submit function', () => {
+        const Constructor = Vue.extend(register);
+        const registerComponent = new Constructor().$mount();
+        assert.typeOf(registerComponent.submit, 'function');
+    })
+
+    it('has a Facebook submit function', () => {
+        const Constructor = Vue.extend(register);
+        const registerComponent = new Constructor().$mount();
+        assert.typeOf(registerComponent.fb, 'function');
+    })
+
+    it('has a Twitter submit function', () => {
+        const Constructor = Vue.extend(register);
+        const registerComponent = new Constructor().$mount();
+        assert.typeOf(registerComponent.twitter, 'function');
+    })
+
+    it('has a Google submit function', () => {
+        const Constructor = Vue.extend(register);
+        const registerComponent = new Constructor().$mount();
+        assert.typeOf(registerComponent.google, 'function');
+    })
   
     it('tests form input and submit button click', () => {
         const Constructor = Vue.extend(register);
