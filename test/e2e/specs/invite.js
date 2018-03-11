a74892d73db3e0cb1c054b1b3bdf8d927df088f9
rhbrian <brianhrhee@gmail.com>
2018-03-11 04:08:08 -0400

    added signin procedure and 2 no input tests for invite

--- test/e2e/specs/invite.js
+++ test/e2e/specs/invite.js
@@ -5,6 +5,13 @@ module.exports = {
 
   /*'Invite: Test Invitation' :client => {
     client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
     .url('http://localhost:8080/invite')
     .waitForElementVisible('#invite', 1000)
     .setValue('input[name=emailField]', 'john@john.com')
@@ -15,6 +22,13 @@ module.exports = {
 
   'Invite: Test Email Clear' :client => {
     client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
     .url('http://localhost:8080/invite')
     .waitForElementVisible('#invite', 1000)
     .setValue('input[name=emailField]', 'john@john.com')
@@ -26,12 +40,54 @@ module.exports = {
 
   'Invite: Test Invalid Email Input' :client => {
     client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
     .url('http://localhost:8080/invite')
     .waitForElementVisible('#invite', 1000)
     .setValue('input[name=emailField]', 'aaa')
-    .pause(5000)
     .click('#submit')
+    .pause(5000)
     .assert.containsText('#invite', 'E-mail must be valid')
+    .end();
+  },
+
+  'Invite: Test No Email Input with setting input fields' :client => {
+    client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
+    .url('http://localhost:8080/invite')
+    .waitForElementVisible('#invite', 1000)
+    .setValue('input[name=emailField]', '')
+    .click('#submit')
+    .pause(5000)
+    .assert.containsText('#invite', 'Must have email to send to')
+    .end();
+  },
+
+  'Invite: Test No Email Input without setting input fields' :client => {
+    client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
+    .url('http://localhost:8080/invite')
+    .waitForElementVisible('#invite', 1000)
+    .click('#submit')
+    .pause(5000)
+    .assert.containsText('#invite', 'Must have email to send to')
     .end();
   },*/
 
