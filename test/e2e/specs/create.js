1cb5e072d98cb1d01884649fa625ee45241832a7
erenwh <erenwh@gmail.com>
2018-03-11 17:41:12 -0400

    update create test

--- test/e2e/specs/create.js
+++ test/e2e/specs/create.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-
-  'Test for creating meets with title empty': client => {
+  /*
+  'Test for creating meets with only title': client => {
     client
       //sign in
       .url('http://localhost:8080/signin')
@@ -20,9 +20,127 @@ module.exports = {
       //-----------From here its to click the create menu
       .click('a[id=create]')
       .pause(3000)
-
+      .assert.urlEquals('http://localhost:8080/create')
       .waitForElementVisible('#createPage', 3000)
       .setValue('input[id="title"]', 'HanTest1')
+      .click('button[id="sub"]')
+      .pause(5000)
+      .assert.containsText('#createPage', 'Description is required')
+      .assert.containsText('#createPage', 'Location is required')
+      .end();
+  },
+  
+  'Test for creating meets with only Description': client => {
+    client
+      //sign in
+      .url('http://localhost:8080/signin')
+      .waitForElementVisible('#signin', 1000)
+      .setValue('input[id=email]', 'han@han.com')
+      .setValue('input[name=pass]', '11111111')
+      .click('button[id=loginBTN]')
+      .pause(5000)
+      .assert.urlEquals('http://localhost:8080/')
+      //create event
+      //.url('http://localhost:8080/create')
+      .pause(1000)
+      //-----------From here its to click the hamburger menu
+      .click('button[id=side]')
+      .pause(3000)
+      //-----------From here its to click the create menu
+      .click('a[id=create]')
+      .pause(3000)
+      .assert.urlEquals('http://localhost:8080/create')
+      .waitForElementVisible('#createPage', 3000)
+      .setValue('textarea[id="description"]', 'HanTestDescrip')
+      .click('button[id="sub"]')
+      .pause(5000)
+      .assert.containsText('#createPage', 'Title is required')
+      .assert.containsText('#createPage', 'Location is required')
+      .end();
+  },
+
+  'Test for creating meets with only Location': client => {
+    client
+      //sign in
+      .url('http://localhost:8080/signin')
+      .waitForElementVisible('#signin', 1000)
+      .setValue('input[id=email]', 'han@han.com')
+      .setValue('input[name=pass]', '11111111')
+      .click('button[id=loginBTN]')
+      .pause(5000)
+      .assert.urlEquals('http://localhost:8080/')
+      //create event
+      //.url('http://localhost:8080/create')
+      .pause(1000)
+      //-----------From here its to click the hamburger menu
+      .click('button[id=side]')
+      .pause(3000)
+      //-----------From here its to click the create menu
+      .click('a[id=create]')
+      .pause(3000)
+      .assert.urlEquals('http://localhost:8080/create')
+      .waitForElementVisible('#createPage', 3000)
+      .setValue('input[id="location"]', "HanTestLocation")
+      .click('button[id="sub"]')
+      .pause(5000)
+      .assert.containsText('#createPage', 'Title is required')
+      .assert.containsText('#createPage', 'Description is required')
+      .end();
+  },
+*/
+  'Test for creating meets with only Title and Description': client => {
+    client
+      //sign in
+      .url('http://localhost:8080/signin')
+      .waitForElementVisible('#signin', 1000)
+      .setValue('input[id=email]', 'han@han.com')
+      .setValue('input[name=pass]', '11111111')
+      .click('button[id=loginBTN]')
+      .pause(5000)
+      .assert.urlEquals('http://localhost:8080/')
+      //create event
+      //.url('http://localhost:8080/create')
+      .pause(1000)
+      //-----------From here its to click the hamburger menu
+      .click('button[id=side]')
+      .pause(3000)
+      //-----------From here its to click the create menu
+      .click('a[id=create]')
+      .pause(3000)
+      .assert.urlEquals('http://localhost:8080/create')
+      .waitForElementVisible('#createPage', 3000)
+      .setValue('input[id="title"]', 'HanTest1')
+      .setValue('textarea[id="description"]', 'HanTestDescrip')
+      .click('button[id="sub"]')
+      .pause(5000)
+      .assert.containsText('#createPage', 'Location is required')
+      .end();
+  },
+
+  'Test for creating meets with title description and location then cancle the event': client => {
+    client
+      //sign in
+      .url('http://localhost:8080/signin')
+      .waitForElementVisible('#signin', 1000)
+      .setValue('input[id=email]', 'han@han.com')
+      .setValue('input[name=pass]', '11111111')
+      .click('button[id=loginBTN]')
+      .pause(5000)
+      .assert.urlEquals('http://localhost:8080/')
+      //create event
+      //.url('http://localhost:8080/create')
+      .pause(1000)
+      //-----------From here its to click the hamburger menu
+      .click('button[id=side]')
+      .pause(3000)
+      //-----------From here its to click the create menu
+      .click('a[id=create]')
+      .pause(3000)
+      .assert.urlEquals('http://localhost:8080/create')
+      .waitForElementVisible('#createPage', 3000)
+      .setValue('input[id="title"]', 'HanTest1')
+      .setValue('textarea[id="description"]', 'HanTestDescrip')
+      .setValue('input[id="location"]', "HanTestLocation")
       .click('button[id="sub"]')
       .pause(5000)
       .assert.urlEquals('http://localhost:8080/orgmeets')
@@ -31,106 +149,15 @@ module.exports = {
       .pause(3000)
       .click('.white--text')
       .pause(3000)
+      .assert.containsText('#meetPage', 'HanTest1')
+      .assert.containsText('#meetPage', 'HanTestDescrip')
+      .assert.containsText('#meetPage', 'HanTestLocation')
+      .pause(1000)
       .click('button[id="cancelBTN"]')
       .pause(1000)
       .assert.urlEquals('http://localhost:8080/')
       .pause(2000)
       .end();
   },
-  'Test for creating meets with only title and location': client => {
-    client
-      //sign in
-      .url('http://localhost:8080/signin')
-      .waitForElementVisible('#signin', 1000)
-      .setValue('input[id=email]', 'han@han.com')
-      .setValue('input[name=pass]', '11111111')
-      .click('button[id=loginBTN]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/')
-      //create event
-      .url('http://localhost:8080/create')
-      .waitForElementVisible('#createPage', 3000)
-      .setValue('input[id="title"]', 'HanTest2')
-      .setValue('input[id="location"]', "HanTestLocation2")
-      .click('button[id="sub"]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/orgmeets')
-      .assert.containsText('#orgMeets', 'HanTest2')
-      //delete event
-      .pause(3000)
-      .click('.white--text')
-      .pause(3000)
-      .assert.containsText('#meetPage', 'HanTestLocation2')
-      .pause(1000)
-      .click('button[id="cancelBTN"]')
-      .pause(1000)
-      .assert.urlEquals('http://localhost:8080/')
-      .end();
-  },
-  'Test for creating meets with few information': client => {
-    client
-      //sign in
-      .url('http://localhost:8080/signin')
-      .waitForElementVisible('#signin', 1000)
-      .setValue('input[id=email]', 'han@han.com')
-      .setValue('input[name=pass]', '11111111')
-      .click('button[id=loginBTN]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/')
-      //create event
-      .url('http://localhost:8080/create')
-      .waitForElementVisible('#createPage', 3000)
-      .setValue('input[id="title"]', 'HanTest3')
-      //.setValue('input[id="description"]', 'HanTestDescrip')
-      .setValue('input[id="location"]', "HanTestLocation3")
-      .click('button[id="sub"]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/orgmeets')
-      .assert.containsText('#orgMeets', 'HanTest3')
-      //delete event
-      .pause(3000)
-      .click('.white--text')
-      .pause(3000)
-      .assert.containsText('#meetPage', 'HanTestLocation3')
-      .pause(1000)
-      .click('button[id="cancelBTN"]')
-      .pause(1000)
-      .assert.urlEquals('http://localhost:8080/')
-      .end();
-  },
-  'Test for creating meets with all information': client => {
-    client
-      //sign in
-      .url('http://localhost:8080/signin')
-      .waitForElementVisible('#signin', 1000)
-      .setValue('input[id=email]', 'han@han.com')
-      .setValue('input[name=pass]', '11111111')
-      .click('button[id=loginBTN]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/')
-      //create event
-      .url('http://localhost:8080/create')
-      .waitForElementVisible('#createPage', 3000)
-      .setValue('input[id="title"]', 'HanTest4')
-      //.setValue('input[id="description"]', 'HanTestDescrip')
-      //upload img
-      /*.click('button[id=uploadBTN]')
-      .setValue('.selector', path.resolve('./assets/logo.png'))
-      .pause(1000)*/
-      .setValue('input[id="location"]', "HanTestLocation4")
-      .click('button[id="sub"]')
-      .pause(5000)
-      .assert.urlEquals('http://localhost:8080/orgmeets')
-      .assert.containsText('#orgMeets', 'HanTest4')
-      //delete event
-      .pause(3000)
-      .click('.white--text')
-      .pause(3000)
-      .assert.containsText('#meetPage', 'HanTestLocation4')
-      .pause(1000)
-      .click('button[id="cancelBTN"]')
-      .pause(1000)
-      .assert.urlEquals('http://localhost:8080/')
-      .end();
-  },
+
 }
