bd91e7e13e5f995d8085116702cfcb7c9cc52b4d
rhbrian <brianhrhee@gmail.com>
2018-03-11 04:07:50 -0400

    added signin procedure and no input tests for donate

--- test/e2e/specs/donate.js
+++ test/e2e/specs/donate.js
@@ -5,6 +5,13 @@ module.exports = {
 
   /*'Donate: Test Donation' :client => {
     client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
     .url('http://localhost:8080/donate')
     .waitForElementVisible('#donate', 1000)
     .setValue('input[name=amount]', '123')
@@ -15,12 +22,37 @@ module.exports = {
 
   'Donate: Test Invalid Donation Input' :client => {
     client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
     .url('http://localhost:8080/donate')
     .waitForElementVisible('#donate', 1000)
     .setValue('input[name=amount]', 'aaa')
     .click('#paypal')
     .pause(5000)
     .assert.containsText('#donate', 'Please enter numbers')
+    .end();
+  },
+
+  'Donate: Test No Donation Input' :client => {
+    client
+    .url('http://localhost:8080/signin')
+    .waitForElementVisible('#signin', 1000)
+    .setValue('input[id=email]', 'test@test.com')
+    .setValue('input[name=pass]', '12345678')
+    .click('button[id=loginBTN]')
+    .pause(5000)
+    .assert.urlEquals('http://localhost:8080/')
+    .url('http://localhost:8080/donate')
+    .waitForElementVisible('#donate', 1000)
+    .setValue('input[name=amount]', '')
+    .click('#paypal')
+    .pause(5000)
+    .assert.containsText('#donate', 'Amount is required')
     .end();
   },*/
 
