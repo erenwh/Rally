1729ced7803c4caa04baf9ee17bab6d72cd9b843
rhbrian <brianhrhee@gmail.com>
2018-03-11 00:43:43 -0500

    testing all elements on invite

---
+++ test/e2e/specs/invite.js
@@ -0,0 +1,38 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+
+  /*'Invite: Test Invitation' :client => {
+    client
+    .url('http://localhost:8080/invite')
+    .waitForElementVisible('#invite', 1000)
+    .setValue('input[name=emailField]', 'john@john.com')
+    .click('#submit')
+    .pause(5000)
+    .end();
+  },
+
+  'Invite: Test Email Clear' :client => {
+    client
+    .url('http://localhost:8080/invite')
+    .waitForElementVisible('#invite', 1000)
+    .setValue('input[name=emailField]', 'john@john.com')
+    .click('#clear')
+    .pause(5000)
+    .assert.containsText('input[name=emailField]', '')
+    .end();
+  },
+
+  'Invite: Test Invalid Email Input' :client => {
+    client
+    .url('http://localhost:8080/invite')
+    .waitForElementVisible('#invite', 1000)
+    .setValue('input[name=emailField]', 'aaa')
+    .pause(5000)
+    .click('#submit')
+    .assert.containsText('#invite', 'E-mail must be valid')
+    .end();
+  },*/
+
+}
