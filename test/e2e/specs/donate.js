b3f9a12eaaf98ba749ba6130a5a378365271d8a1
rhbrian <brianhrhee@gmail.com>
2018-03-11 00:43:18 -0500

    testing all elements on donate

---
+++ test/e2e/specs/donate.js
@@ -0,0 +1,27 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+
+  /*'Donate: Test Donation' :client => {
+    client
+    .url('http://localhost:8080/donate')
+    .waitForElementVisible('#donate', 1000)
+    .setValue('input[name=amount]', '123')
+    .click('#paypal')
+    .pause(5000)
+    .end();
+  },
+
+  'Donate: Test Invalid Donation Input' :client => {
+    client
+    .url('http://localhost:8080/donate')
+    .waitForElementVisible('#donate', 1000)
+    .setValue('input[name=amount]', 'aaa')
+    .click('#paypal')
+    .pause(5000)
+    .assert.containsText('#donate', 'Please enter numbers')
+    .end();
+  },*/
+
+}
