47c51aee01126999ddfa26776fe8f9cc3016dcc2
erenwh <erenwh@gmail.com>
2018-03-11 22:40:38 -0400

    orgmeets test finished

--- test/e2e/specs/orgMeets.js
+++ test/e2e/specs/orgMeets.js
@@ -1,5 +1,6 @@
-//module.exports = {
-    /*'Test for opening organized meet': client => {
+/*
+module.exports = {
+    'Test for opening organized meet': client => {
         client
         //signin
             .url('http://localhost:8080/signin')
@@ -20,42 +21,11 @@
             .waitForElementVisible('#orgMeets', 3000)
             .click('.white--text')
             .pause(3000)
-            .assert.containsText('#meetPage', 'HanDescription1')
             .assert.containsText('#meetPage', 'DO NOT DELETE FOR TESTING')
             .end();
-    },*/
-    /*'Test for edit organized meet': client => {
-        client
-            //signin
-            .url('http://localhost:8080/signin')
-            .waitForElementVisible('#signin', 1000)
-            .setValue('input[id=email]', 'orgmeettest@test.com')
-            .setValue('input[name=pass]', '11111111')
-            .click('button[id=loginBTN]')
-            .pause(5000)
-            .assert.urlEquals('http://localhost:8080/')
-            .pause(1000)
-            .click('button[id=side]')
-            .pause(3000)
-            .click('a[id=org]')
-            .pause(3000)
-            .assert.urlEquals('http://localhost:8080/orgmeets')
-            .waitForElementVisible('#orgMeets', 3000)
-            .click('button[id="editBTN"]')
-            .pause(1000)
-            .waitForElementVisible('#editEvent', 1000)
-            .pause(5000)
-            //clear
-            .clearValue('textarea[id="description"]')
-            .clearValue('input[id="location"]')
-            //change description and location
-            .setValue('textarea[id="description"]', 'HanDescription1-EDIT')
-            .pause(500)
-            .setValue('input[id="location"]', 'DO NOT DELETE FOR TESTING-EDIT')
-            .pause(5000)
-            .end();
-    },*/
-    /*'Test for edit then closing an organized meet wihtout change': client => {
+    },
+
+    'Test for edit then closing an organized meet wihtout change': client => {
         client
             //signin
             .url('http://localhost:8080/signin')
@@ -85,5 +55,42 @@
             .assert.containsText('#orgMeets', '2018-03-15')
             .assert.containsText('#orgMeets', '20:39')
             .end();
-    }*/
-//}
+    },
+
+    'Test for edit organized meet': client => {
+        client
+        .url('http://localhost:8080/signin')
+        .waitForElementVisible('#signin', 1000)
+        .setValue('input[id=email]', 'orgmeettest@test.com')
+        .setValue('input[name=pass]', '11111111')
+        .click('button[id=loginBTN]')
+        .pause(3000)
+        .assert.urlEquals('http://localhost:8080/')
+        .pause(1000)
+        .click('button[id=side]')
+        .pause(3000)
+        .click('a[id=org]')
+        .pause(3000)
+        .assert.urlEquals('http://localhost:8080/orgmeets')
+        .waitForElementVisible('#orgMeets', 3000)
+        .click('button[id=editBTN]')
+        .pause(3000)
+
+        //set
+        .setValue('textarea[id="description"]', 'test/')
+        .pause(3000)
+        .setValue('input[id="location"]', 'test2/')
+        .pause(3000)
+        .execute(function () {
+            document.querySelector('#sbtn').click()
+        })
+        .pause(3000)
+        //.assert.containsText('#editEvent', 'HanDescription2')
+        .click('.white--text')
+        .pause(3000)
+        .assert.containsText('#meetPage', 'test/')
+        .assert.containsText('#meetPage', 'test2/')
+        .end();
+},
+}
+*/
